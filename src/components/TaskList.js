ff5be0a96dc66f5879eb56f0b7ebba2ad442074a
Jakub-Zygmunt <jakub.zygmunt@htdevelopers.com>
2021-04-25 10:19:51 +0200

    fourth chapter

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Task from "./Task";
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
+
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -51,3 +55,19 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
     </div>
   );
 }
+
+PureTaskList.defaultProps = {
+  loading: false,
+};
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
